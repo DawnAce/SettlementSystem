6cf01423a0c9ad436696fa7da616febc9cbe2aed
Xi Chen <xichen5@microsoft.com>
2019-09-20 12:21:46 +0800

    Add aggregation show function

---
+++ SettlementSystem/Models/PO/DisplayRulesPO.cs
@@ -0,0 +1,21 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SettlementSystem.Models.PO
+{
+    [SugarTable("DisplayRules")]
+    public class DisplayRulesPO
+    {
+        [SugarColumn(IsPrimaryKey = true)]
+        public string Id { get; set; }
+
+        public string Pre { get; set; }
+
+        public string Next { get; set; }
+
+        public bool IsDisplay { get; set; }
+    }
+}
--- SettlementSystem/Service/CalculateService.cs
+++ SettlementSystem/Service/CalculateService.cs
@@ -20,74 +20,54 @@ namespace SettlementSystem.Service
         public static MyResponse CalculateFee(string[] ksIds, string[] feeTypes, string[] dates)
         {
             var db = SugarDao.GetInstance();
-            var orgMap = new Dictionary<string, JsonObject>();
-            db.Queryable<OrganizationPO>()
-                .ToList()
-                .ForEach(x => {
-                    orgMap[x.Id] = new JsonObject { ["Name"] = x.Name };
-                    if (x.Type != 3)
-                        orgMap[x.Id]["Children"] = new List<string>();
-                    //将自己记录到父节点，防止出现本月没人挂号，不显示统计的问题
-                    if (x.Type != 1)
-                        ((List<string>)(orgMap[x.Id.Substring(0, (x.Type - 1) * 3)]["Children"])).Add(x.Id);
-                });
-
-            var ksSet = new HashSet<string>(ksIds);
-            var dateSet = new HashSet<string>(dates);
-            var typeSet = new HashSet<string>(feeTypes);
-            var temp = new List<TempCalculateYyxx>();
-            db.Queryable<YyxxPO>()
-                .Where(x => dateSet.Contains(x.Id.Substring(0, 6)))
-                .Where(x => ksSet.Contains(x.Ksczid))
-                .Where(x => typeSet.Contains(x.Jsqd))
-                .GroupBy(x => x.Ddzt).GroupBy(x => x.Ksczid).GroupBy(x => x.Jsqd)
-                .Select(x => new { ksczid = x.Ksczid, ddzt = x.Ddzt, jsqd = x.Jsqd, count = SqlFunc.AggregateCount(1) })
-                .ToList()
-                .ForEach(x => temp.Add(
-                    new TempCalculateYyxx {
-                        Ksczid = x.ksczid, Ddzt = ParseDdzt(x.ddzt), Jsqd = x.jsqd, Count = x.count
-                    })
-                );
-
-            var ruleMap = new Dictionary<string, NormalRulesPO>();
-            db.Queryable<NormalRulesPO>()
-                .Where(x => ksSet.Contains(x.Id.Substring(0, 9)))
-                .ToList()
-                .ForEach(x => ruleMap[x.Id] = x);
+            var orgMap = GetOrgMap(db);
+            var temp = GetFliterResult(db, ksIds, dates, feeTypes);
+            var ruleMap = GetNormalRuls(db, ksIds);
+            var displayRuleMap = GetDisplayRules(db);
 
             var resultList = new List<HospitalVO>();
             var resultMap = new Dictionary<string, HospitalVO>();
-            foreach(var obj in temp)
+            foreach (var obj in temp)
             {
                 var zyId = obj.Ksczid.Substring(0, 3);
                 var fyId = obj.Ksczid.Substring(0, 6);
                 HospitalVO zyObj, fyObj;
                 if (!resultMap.ContainsKey(zyId))
                 {
-                    resultMap[zyId] = new HospitalVO { Id = zyId, Zymc = (string)orgMap[zyId]["Name"]};
+                    resultMap[zyId] = new HospitalVO { Id = zyId, Zymc = (string)orgMap[zyId]["Name"] };
                     resultList.Add(resultMap[zyId]);
                 }
                 zyObj = resultMap[zyId];
 
                 if (!resultMap.ContainsKey(fyId))
                 {
-                    resultMap[fyId] = new HospitalVO { Id = fyId, Fymc = (string)orgMap[fyId]["Name"]};
+                    resultMap[fyId] = new HospitalVO { Id = fyId, Fymc = (string)orgMap[fyId]["Name"] };
                     if (zyObj.Children == null)
                         zyObj.Children = new List<HospitalVO>();
                     zyObj.Children.Add(resultMap[fyId]);
-                    //如果组织结构中有科室，先创建所有科室，防止没人挂号，导致不显示的问题
-                    if(((List<string>)(orgMap[fyId]["Children"])).Count != 0)
+                    //如果组织结构中有分院，先创建所有科室，防止没人挂号，导致不显示的问题
+                    if (((List<string>)(orgMap[fyId]["Children"])).Count != 0)
                     {
                         resultMap[fyId].Children = new List<HospitalVO>();
                         ((List<string>)(orgMap[fyId]["Children"]))
                             .ForEach(x => {
-                                var tempKsObj = new HospitalVO { Id = x, Ks = (string)(orgMap[x]["Name"])};
-                                resultMap[fyId].Children.Add(tempKsObj);
-                                resultMap[x] = tempKsObj;
+                                if (!resultMap.ContainsKey(x))
+                                {
+                                    var tempKsObj = new HospitalVO { Id = x, Ks = (string)(orgMap[x]["Name"]) };
+                                    resultMap[fyId].Children.Add(tempKsObj);
+                                    resultMap[x] = tempKsObj;
+
+                                    // 如果对象需要与其他科室一起展示，那么进行遍历
+                                    if (displayRuleMap.ContainsKey(x))
+                                    {
+                                        SetDiplayDepartment(resultMap, x, displayRuleMap, orgMap, true);
+                                        SetDiplayDepartment(resultMap, x, displayRuleMap, orgMap, false);
+                                    }
+                                }
                             });
                     }
                 }
-                    
+
                 fyObj = resultMap[fyId];
                 var fee = ruleMap.ContainsKey(obj.Ksczid + obj.Ddzt) ? ruleMap[obj.Ksczid + obj.Ddzt].Fee : 0;
                 var ksObj = resultMap[obj.Ksczid];
@@ -102,17 +82,17 @@ namespace SettlementSystem.Service
             }
 
             // Sort
-            foreach(var item in resultMap)
+            foreach (var item in resultMap)
             {
                 if (item.Value.Children == null)
                     continue;
                 item.Value.Children.Sort((x, y) => (int)(y.Ze - x.Ze));
             }
-            resultList.Sort((x,y) => (int)(y.Ze - x.Ze));
+            resultList.Sort((x, y) => (int)(y.Ze - x.Ze));
             //如果只有一个分院，那么去掉一个层级
             resultList.ForEach(item =>
             {
-                if(item.Children.Count == 1)
+                if (item.Children.Count == 1)
                 {
                     var fymc = item.Children[0].Fymc;
                     item.Children = item.Children[0].Children;
@@ -121,6 +101,90 @@ namespace SettlementSystem.Service
             });
 
             return MyResponse.Success(resultList);
+        }
+
+        private static Dictionary<string, JsonObject> GetOrgMap(SqlSugarClient db)
+        {
+            var orgMap = new Dictionary<string, JsonObject>();
+            db.Queryable<OrganizationPO>()
+                .ToList()
+                .ForEach(x => {
+                    orgMap[x.Id] = new JsonObject { ["Name"] = x.Name, ["Id"] = x.Id };
+                    if (x.Type != 3)
+                        orgMap[x.Id]["Children"] = new List<string>();
+                    //将自己记录到父节点，防止出现本月没人挂号，不显示统计的问题
+                    if (x.Type != 1)
+                        ((List<string>)(orgMap[x.Id.Substring(0, (x.Type - 1) * 3)]["Children"])).Add(x.Id);
+                });
+            return orgMap;
+        }
+
+        private static List<TempCalculateYyxx> GetFliterResult(SqlSugarClient db, string[] ksIds, string[] dates, string[] feeTypes)
+        {
+            var ksSet = new HashSet<string>(ksIds);
+            var dateSet = new HashSet<string>(dates);
+            var typeSet = new HashSet<string>(feeTypes);
+            var temp = new List<TempCalculateYyxx>();
+            db.Queryable<YyxxPO>()
+                .Where(x => dateSet.Contains(x.Id.Substring(0, 6)))
+                .Where(x => ksSet.Contains(x.Ksczid))
+                .Where(x => typeSet.Contains(x.Jsqd))
+                .GroupBy(x => x.Ddzt).GroupBy(x => x.Ksczid).GroupBy(x => x.Jsqd)
+                .Select(x => new { ksczid = x.Ksczid, ddzt = x.Ddzt, jsqd = x.Jsqd, count = SqlFunc.AggregateCount(1) })
+                .ToList()
+                .ForEach(x => temp.Add(
+                    new TempCalculateYyxx
+                    {
+                        Ksczid = x.ksczid,
+                        Ddzt = ParseDdzt(x.ddzt),
+                        Jsqd = x.jsqd,
+                        Count = x.count
+                    })
+                );
+            return temp;
+        }
+
+        private static Dictionary<string, NormalRulesPO> GetNormalRuls(SqlSugarClient db, string[] ksIds)
+        {
+            var ruleMap = new Dictionary<string, NormalRulesPO>();
+            var ksSet = new HashSet<string>(ksIds);
+            db.Queryable<NormalRulesPO>()
+                .Where(x => ksSet.Contains(x.Id.Substring(0, 9)))
+                .ToList()
+                .ForEach(x => ruleMap[x.Id] = x);
+            return ruleMap;
+        }
+
+        private static Dictionary<string, DisplayRulesPO> GetDisplayRules(SqlSugarClient db)
+        {
+            var displayRuleMap = new Dictionary<string, DisplayRulesPO>();
+            db.Queryable<DisplayRulesPO>()
+                .Where(x => x.IsDisplay)
+                .ToList()
+                .ForEach(x => displayRuleMap[x.Id] = x);
+            return displayRuleMap;
+        }
+
+        // 将组合一起显示的科室对象进行显示
+        private static void SetDiplayDepartment(Dictionary<string, HospitalVO> resultMap, string currentKey, 
+            Dictionary<string, DisplayRulesPO> displayRules, Dictionary<string, JsonObject> orgMap, bool down)
+        {
+            var obj = resultMap[currentKey];
+            string nextSearchKey = down ? displayRules[currentKey].Next : displayRules[currentKey].Pre;
+            if (nextSearchKey == null) return;
+
+            resultMap[nextSearchKey] = obj;
+            if (down)
+            {
+                obj.Ks += "," + orgMap[nextSearchKey]["Name"];
+                obj.Id += "," + orgMap[nextSearchKey]["Id"];
+            }
+            else
+            {
+                obj.Ks = orgMap[nextSearchKey]["Name"] + "," + obj.Ks;
+                obj.Id = orgMap[nextSearchKey]["Id"] + "," + obj.Id;
+            }
+            SetDiplayDepartment(resultMap, nextSearchKey, displayRules, orgMap, down);
         }
 
         private static string ParseDdzt(string ddzt)
@@ -132,9 +196,9 @@ namespace SettlementSystem.Service
 
         private static void AddCount(HospitalVO target, HospitalVO source, string ddzt)
         {
-            if(ddzt.Equals("qx"))
+            if (ddzt.Equals("qx"))
                 target.Qxl += source.Qxl;
-            else if(ddzt.Equals("wdz"))
+            else if (ddzt.Equals("wdz"))
                 target.Wjzl += source.Wjzl;
             else
                 target.Jzl += source.Jzl;
@@ -144,7 +208,7 @@ namespace SettlementSystem.Service
 
         public static HospitalVO GetObj(TempCalculateYyxx obj, double ze)
         {
-            var newObj = new HospitalVO { Id = obj.Ksczid , Qd = obj.Jsqd, Ze = ze};
+            var newObj = new HospitalVO { Id = obj.Ksczid, Qd = obj.Jsqd, Ze = ze };
             if (obj.Ddzt.Equals("qx"))
                 newObj.Qxl = obj.Count;
             else if (obj.Ddzt.Equals("yjz"))
